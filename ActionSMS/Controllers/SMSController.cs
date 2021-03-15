bf8489dc2376e9c969ae50842169d2e6d616ec47
trinhlh96 <trinhlhth1908021@fpt.edu.vn>
2021-03-15 19:45:45 +0700

    first commit

---
+++ ActionSMS/Controllers/SMSController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Twilio;
+using Twilio.AspNet.Mvc;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
+
+namespace ActionSMS.Controllers
+{
+    public class SMSController : TwilioController
+    {
+        // GET: SMS
+        public ActionResult SendSms(string code)
+        {
+            var accountID = "ACe39a71f89650340a9aa237831bfc0922";
+            var authToken = "111d105ea27d5137032d6f3b8f519a85";
+            TwilioClient.Init(accountID, authToken);
+
+            var to = new PhoneNumber("+84911549898");
+            var from = new PhoneNumber("+14073260480");
+
+            var message = MessageResource.Create(
+                to: to,
+                from: from,
+                body: code
+                );
+            return Content(message.Sid);
+        }
+    }
+}
---
+++ ActionSMS/Models/Customer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ActionSMS.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Code { get; set; }
+        public CustomerStatus Status { get; set; }
+    }
+    public enum CustomerStatus
+    {
+        Deativate,Active 
+    }
+}
